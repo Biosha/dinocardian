d8a5550d2f9d24c95154f86385d69455cd40c8f8
Jérémy <jeremy.guilbert@stormshield.eu>
2023-09-18 11:36:07 +0200

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import app from './app';
+import cors from "cors";
+
+const allowedOrigins = '*';
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
 
 app.listen(3333);
