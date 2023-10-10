9dfba7c052d4093733ec102b0ba788dbcc36ea1a
Biosha <guegui@ymail.com>
2023-10-10 23:02:06 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
 import app from './app';
-import cors from "cors";
-
-const allowedOrigins = ['http://www.dinocard.net', '*'];
-
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-};
 
 app.use(cors(options));
 
